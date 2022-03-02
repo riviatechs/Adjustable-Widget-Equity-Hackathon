c7e09441164b57928dcfbc0a7aa9b2555897827d
FrankRight <frankbosire2017@gmail.com>
2022-03-02 12:08:33 +0300

    project init

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import React from "react";
+
+function Header() {
+  return (
+    <Head>
+      <link rel="apple-touch-icon" href="/favicon.ico" />
+      <meta
+        name="description"
+        content="Equity hackathon - configurable MT940 widget"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+    </Head>
+  );
+}
+
+export default Header;
